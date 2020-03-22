b1ff2eb2491318f3f972f9152b3b133d2a4b91be
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-22 23:57:08 +0000

    init commit

---
+++ dbschema.js
@@ -0,0 +1,23 @@
+let db = {
+    users: [
+        {
+            userId: 'n3vt8YG2VPOqjmwZRBzo7iXs2DT2',
+            email: 'fof@email.com',
+            handle: 'fof',
+            createdAt: '2020-03-22T14:04:54.827Z',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/mommyslittlehelper-f4997.appspot.com/o/no-img.png?alt=media',
+            bio: 'Hello, my name is fof, nice to meet you',
+            website: 'https://fof.com',
+            location: 'London, UK'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-03-20T13:11:28.213Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBoj_BFO4UgoXV0i3c7WN9Q6zU4e0xYtd8",
+    authDomain: "mommyslittlehelper-f4997.firebaseapp.com",
+    databaseURL: "https://mommyslittlehelper-f4997.firebaseio.com",
+    projectId: "mommyslittlehelper-f4997",
+    storageBucket: "mommyslittlehelper-f4997.appspot.com",
+    messagingSenderId: "480688436150",
+    appId: "1:480688436150:web:8f0d15a90a8f847ba44717",
+    measurementId: "G-68X2GBBTCS"
+  };
