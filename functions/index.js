4885f46f518009f33f621a1afdaf4dea5f524a28
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-22 23:57:08 +0000

    init commit

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+
+const app = require('express')();
+
+const FBAuth = require('./util/fbAuth');
+
+const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { signup, login, uploadImage } = require('./handlers/users');
+
+//Scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+
+//User routes
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
