374232d354f00f39a4648b773b654828c06e8f57
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-23 16:20:14 +0000

    like, unlike, delete screams

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,14 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, 
+    postOneScream, 
+    getScream, 
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+} = require('./handlers/screams');
 const { signup, 
         login, 
         uploadImage, 
@@ -15,6 +22,11 @@ const { signup,
 //Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //User routes
 app.post('/signup', signup);
@@ -22,5 +34,6 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
