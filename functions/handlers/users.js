9eb23d80aae66d730433b5bf8ae6fa26eee92843
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-24 12:47:38 +0000

    getting user details and read notifications

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -117,7 +117,44 @@ exports.addUserDetails = (req, res) => {
         .catch(err => {
             console.err(err);
             return res.status(500).json({error: err.code})
+        });
+};
+
+// Get any user's details
+exports.getUserDetails = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.params.handle}`).get()
+        .then((doc) => {
+            if(doc.exists){
+                userData.user = doc.data();
+                return db
+                    .collection('screams')
+                    .where('userHandle', '==', req.params.handle)
+                    .orderBy('createdAt', 'desc')
+                    .get();
+            } else {
+                return res.status(404).json({ error: "User not found!"})
+            }
         })
+        .then((data) => {
+            userData.screams = [];
+            data.forEach((doc )=> {
+                userData.screams.push({
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                    userHandle: doc.data().userHandle,
+                    userImage: doc.data().userImage,
+                    likeCount: doc.data().likeCount,
+                    commentCount: doc.data().commentCount,
+                    screamId: doc.id
+                });
+            });
+            return res.json(userData);
+        })
+        .catch(err =>{
+            console.error(err);
+            return res.status(500).json({ error: err.code})
+        });
 }
 
 //Get own user details
@@ -130,10 +167,26 @@ exports.getAuthenticatedUser = (req, res) => {
             return db.collection('likes').where('userHandle', '==', req.user.handle).get()
             }
         })
-        .then(data => {
+        .then(data => {``
             userData.likes = [];
             data.forEach(doc => {
                 userData.likes.push(doc.data());
+            });
+            return db.collection('notifications').where('recipient', '==', req.user.handle)
+                .orderBy('createdAt', 'desc').limit(10).get();
+        })
+        .then(data => {
+            userData.notifications = [];
+            data.forEach(doc => {
+                userData.notifications.push({
+                    recipient: doc.data().recipient,
+                    sender: doc.data().sender,
+                    createdAt: doc.data().createdAt,
+                    screamId: doc.data().screamId,
+                    type: doc.data().type,
+                    read: doc.data().read,
+                    notificationId: doc.id
+                });
             });
             return res.json(userData);
         })
@@ -187,4 +240,20 @@ exports.uploadImage = (req, res) => {
         });
     });
     busboy.end(req.rawBody);
+}
+// Mark notifications as read
+exports.markNotificationsRead = (req, res) => {
+    let batch = db.batch();
+    req.body.forEach(notificationId => {
+        const notification = db.doc(`/notifications/${notificationId}`)
+        batch.update(notification, { read: true });
+    });
+    batch.commit()
+        .then(() => {
+            return res.json({message: "Notifications marked read!"})
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code});
+        })
 }
