484522b404fbcaac13512dc5a05cfd41d43b16e9
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-26 14:09:29 +0000

    added login page and redirect

--- mommys-little-helper-client/src/components/Scream.js
+++ mommys-little-helper-client/src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
@@ -14,7 +16,7 @@ const styles = {
     marginBottom: 20
   },
   image: {
-    minWidth: 160
+    minWidth: 200
   },
   content: {
     padding: 25,
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -52,7 +55,7 @@ class Scream extends Component {
           >
             {userHandle}
           </Typography>
-          <Typography variant="body2">{createdAt}</Typography>
+          <Typography variant="body2">{dayjs(createdAt).fromNow()}</Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
