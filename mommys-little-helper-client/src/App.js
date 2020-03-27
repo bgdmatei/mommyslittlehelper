845b235bf235652450484f0ff6cd0488ba892112
bogdanmatei <matei90bogdan@yahoo.com>
2020-03-27 18:32:20 +0000

    implemented redux

--- mommys-little-helper-client/src/App.js
+++ mommys-little-helper-client/src/App.js
@@ -3,47 +3,61 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 // Components
 import Navbar from "./components/Navbar";
-
+import AuthRoute from "./util/AuthRoute";
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#f9c8d9",
-      main: "#f8bbd0"
-    },
-    secondary: {
-      main: "#ff4081",
-      light: "#ff669a"
-    }
-  },
-  typography: {
-    useNextVariants: true
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
-});
+}
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
+        <Provider store={store}>
           <Router>
             <Navbar />
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <Route exact path="/login" component={login} />
-                <Route exact path="/signup" component={signup} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
       </MuiThemeProvider>
     );
   }
---
+++ mommys-little-helper-client/src/util/theme.js
@@ -0,0 +1,40 @@
+export default {
+  palette: {
+    primary: {
+      main: "#f8bbd0"
+    },
+    secondary: {
+      main: "#ff4081"
+    }
+  },
+  spreadThis: {
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: "center"
+    },
+    image: {
+      margin: "20px auto 20px auto",
+      height: 60
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto"
+    },
+    textField: {
+      margin: "10px auto 10px auto"
+    },
+    button: {
+      margin: "20px auto 20px auto",
+      position: "relative"
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10
+    },
+    progress: {
+      position: "absolute"
+    }
+  }
+};
