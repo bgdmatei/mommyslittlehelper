ac00a223f2b0c7b14e8a64d118aa3b9727e76bff
bogdanmatei <matei90bogdan@yahoo.com>
2020-04-01 00:55:16 +0100

    fixed liked button bug and  added posting comment

---
+++ mommys-little-helper-client/src/components/PostScream.js
@@ -0,0 +1,138 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../util/MyButton";
+//MUI Stuff
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  CircularProgress
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
+//Redux Stuff
+import { connect } from "react-redux";
+import { postScream } from "../redux/actions/dataActions";
+
+const styles = theme => ({
+  ...theme.spreadThis,
+  submitButton: {
+    position: "relative"
+  },
+  progressSpinner: {
+    position: "absolute"
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%",
+    top: "10%"
+  }
+});
+class PostScream extends Component {
+  state = {
+    open: false,
+    body: "",
+    errors: {}
+  };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({
+        errors: nextProps.UI.errors
+      });
+    }
+    if (!nextProps.UI.errors && !nextProps.UI.loading) {
+      this.setState({ body: "" });
+      this.handleClose();
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+  handleClose = () => {
+    this.setState({ open: false, errors: {} });
+  };
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.postScream({ body: this.state.body });
+  };
+  render() {
+    const { errors } = this.state;
+    const {
+      classes,
+      UI: { loading }
+    } = this.props;
+    return (
+      <Fragment>
+        <MyButton onClick={this.handleOpen} tip="Post a comment">
+          <AddIcon />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <MyButton
+            tip="Close"
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogTitle>Post a new comment </DialogTitle>
+          <DialogContent>
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                name="body"
+                type="text"
+                label="Comment!"
+                rows="3"
+                placeholder="Write something"
+                error={errors.body ? true : false}
+                helperText={errors.body}
+                className={classes.textField}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                disabled={loading}
+              >
+                Submit
+                {loading && (
+                  <CircularProgress
+                    size={25}
+                    className={classes.progressSpinner}
+                  />
+                )}
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
+
+PostScream.propTypes = {
+  postScream: PropTypes.func.isRequired,
+  UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  UI: state.UI
+});
+
+export default connect(mapStateToProps, { postScream })(
+  withStyles(styles)(PostScream)
+);
