a2e3ecdae5907240137e425bd3b39d3ce389d218
bogdanmatei <matei90bogdan@yahoo.com>
2020-04-04 20:20:17 +0100

    adding comments to posts

--- mommys-little-helper-client/src/components/scream/PostScream.js
+++ mommys-little-helper-client/src/components/scream/PostScream.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
-import MyButton from "../util/MyButton";
+import MyButton from "../../util/MyButton";
 //MUI Stuff
 import {
   Button,
@@ -15,7 +15,7 @@ import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from "@material-ui/icons/Close";
 //Redux Stuff
 import { connect } from "react-redux";
-import { postScream, clearErrors } from "../redux/actions/dataActions";
+import { postScream, clearErrors } from "../../redux/actions/dataActions";
 
 const styles = theme => ({
   ...theme.spreadThis,
